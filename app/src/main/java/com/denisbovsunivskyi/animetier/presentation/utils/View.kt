2509bd05c515ea21d9c771a01416407a96834b38
Denis Bovsunivskyi <wweqtv1@gmail.com>
2023-04-11 18:04:36 +0300

    delete unused fun, fix dagger warning

--- app/src/main/java/com/denisbovsunivskyi/animetier/presentation/utils/View.kt
+++ app/src/main/java/com/denisbovsunivskyi/animetier/presentation/utils/View.kt
@@ -1,11 +1,7 @@
 package com.denisbovsunivskyi.animetier.presentation.utils
 
-import android.app.Activity
 import android.content.Context
-import android.graphics.Color
-import android.os.Build
 import android.view.View
-import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 
 fun View.showView() {
@@ -28,57 +24,16 @@ fun View.hideKeyboard() {
 
 fun View.setHideKeyboardWrapperListener() {
     this.setOnFocusChangeListener { view, hasFocus ->
-        if(hasFocus) {
+        if (hasFocus) {
             view.hideKeyboard()
         }
     }
 }
 
-fun View.setOnClickListenerWithDebounce(delay:Long = 1000, action:()->Unit) {
+fun View.setOnClickListenerWithDebounce(delay: Long = 1000, action: () -> Unit) {
     this.setOnClickListener {
         this.isEnabled = false
-        this.postDelayed( { this.isEnabled = true }, delay)
+        this.postDelayed({ this.isEnabled = true }, delay)
         action.invoke()
     }
-}
-fun Activity.transparentStatusAndNavigation(
-    systemUiScrim: Int = Color.parseColor("#40000000") // 25% black
-) {
-    var systemUiVisibility = 0
-    // Use a dark scrim by default since light status is API 23+
-    var statusBarColor = systemUiScrim
-    //  Use a dark scrim by default since light nav bar is API 27+
-    var navigationBarColor = systemUiScrim
-    val winParams = window.attributes
-
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        systemUiVisibility = systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-        statusBarColor = Color.TRANSPARENT
-    }
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        systemUiVisibility = systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-        navigationBarColor = Color.TRANSPARENT
-    }
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-        systemUiVisibility = systemUiVisibility or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        window.decorView.systemUiVisibility = systemUiVisibility
-    }
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-        winParams.flags = winParams.flags or
-                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or
-                WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
-    }
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        winParams.flags = winParams.flags and
-                (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or
-                        WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION).inv()
-        window.statusBarColor = statusBarColor
-        window.navigationBarColor = navigationBarColor
-    }
-
-    window.attributes = winParams
 }
